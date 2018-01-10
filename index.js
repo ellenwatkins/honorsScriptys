75c8b763f9c495bbb90822443d8e2d8b660aec83
Ellen Watkins <ellen@scripted.org>
2018-01-10 08:13:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
- 
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script); 
-
 $("#bestMovieWinner").click(function(){
     $(".bestMovie").slideUp();
     $("#movie1").slideDown();
