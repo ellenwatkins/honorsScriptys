8c9b6d57f69e36bd335b72047e0e809c34914b11
Ellen Watkins <ellen@scripted.org>
2018-01-09 18:35:43 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+$("#bestMovieWinner").click(function(){
+    $(".bestMovie").slideUp();
+    $("#movie1").slideDown();
+});
+
+$("#bestArtistWinner").click(function(){
+    $(".bestArtist").slideUp();
+    $("#artist2").slideDown();
+});
+
+$("#bestGameWinner").click(function(){
+    $(".bestGame").slideUp();
+    $("#game3").slideDown();
+    $("#jk").text("RIGHT??");
+});
